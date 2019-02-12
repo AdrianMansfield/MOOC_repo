cb4fc2c081192eb3821af2f8ebf9cc6e91ade41a
Andrei Mayorau <amayorau@gmail.com>
2019-02-12 11:54:58 +0300

    Initial commit

---
+++ src/main/java/com/project/mooc/moocproject/MoocProjectApplication.java
@@ -0,0 +1,14 @@
+package com.project.mooc.moocproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoocProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoocProjectApplication.class, args);
+    }
+
+}
+
